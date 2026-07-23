91e1641f5b09776a129693bb3f7c7cc5c911d911
rayone <rayone@users.noreply.github.com>
2026-07-23 16:27:56 +0800

    subMission v1

---
+++ Casks/submission.rb
@@ -0,0 +1,23 @@
+cask "submission" do
+  version "1"
+  sha256 "843553e074c052f136f779e3e516c917ab2bdb675096f19e62a34c8c1bab4f5f"
+
+  url "https://github.com/rayone/subMission/releases/download/v#{version}/subMission.dmg"
+  name "subMission"
+  desc "Native macOS client for Transmission"
+  homepage "https://github.com/rayone/subMission"
+
+  depends_on macos: ">= :ventura"
+
+  app "subMission.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/subMission.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Preferences/subMission.plist",
+  ]
+end
